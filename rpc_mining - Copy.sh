cb9c3a06bbd0a93186b3ccd3146346d25d7df7c7
neccd88 <130989703+neccd88@users.noreply.github.com>
2025-07-09 00:13:55 +0700

    Add files via upload

---
+++ rpc_mining - Copy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Replace YOUR_ADDRESS, YOUR_NODE:YOUR_PORT to run the miner"
+while :; do
+    ./astrominer -w deroi1qyzlxxgq2weyqlxg5u4tkng2lf5rktwanqhse2hwm577ps22zv2x2q9pvfz92xmrwu8d7kh3vyqsm4h307 -r stue.threepool.tech:3030 -m 8 -p rpc;
+    sleep 5;
+done
